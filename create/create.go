386286f62eed52cb8967f2f4a019062cb4c7d94e
walkerwmanuel <walkerwmanuel@gmail.com>
2023-02-21 18:21:51 -0500

    Finalized initial logic

--- create/create.go
+++ create/create.go
@@ -4,6 +4,7 @@ import (
 	"walkerwmanuel/blackjack/types"
 )
 
+// CreatePlayer - takes in a username and password and put it into a pointer of a player struct
 func CreatePlayer(u, p string) *types.Player {
 
 	newPlayer := types.Player{}
@@ -12,4 +13,25 @@ func CreatePlayer(u, p string) *types.Player {
 	newPlayer.Password = p
 
 	return &newPlayer
+}
+
+// CreateGame - takes in an id and returns a game with a a blank non nil array of players
+func CreateGame(i int) *types.Game {
+
+	newGame := types.Game{}
+
+	newGame.Id = i
+
+	return &newGame
+}
+
+// AddPlayerToGame - Takes in game and player struct, adds the player to the game struct in its player value
+func AddPlayerToGame(newPlayer *types.Player, newGame *types.Game) *types.Game {
+
+	newGameWithPlayer := types.Game{}
+
+	newGameWithPlayer.Id = newGame.Id
+	newGameWithPlayer.Player = newPlayer
+
+	return &newGameWithPlayer
 }
--- main.go
+++ main.go
@@ -1,9 +1,60 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
+	"os"
+	"walkerwmanuel/blackjack/create"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	//Ask for user input on username
+	fmt.Print("Enter username: ")
+	input := bufio.NewReader(os.Stdin)
+	username, err := input.ReadString('\n')
+	if err != nil {
+		fmt.Println("An error occured while reading input. Please try again", err)
+		return
+	}
+
+	//Ask for user input on password
+	fmt.Print("Enter password: ")
+	input2 := bufio.NewReader(os.Stdin)
+	password, err := input2.ReadString('\n')
+	if err != nil {
+		fmt.Println("An error occured while reading input. Please try again", err)
+		return
+	}
+
+	x := create.CreatePlayer(username, password)
+
+	fmt.Printf("Created Player: %v ", x)
+
+	y := create.CreateGame(1)
+
+	z := create.AddPlayerToGame(x, y)
+
+	z1, err := json.Marshal(z)
+	if err != nil {
+		fmt.Println("Error trynna do that json")
+	}
+
+	fmt.Printf("Game with player added to it: %v", z1)
+
 }
+
+// func main2() {
+// 	fmt.Print("Enter text: ")
+// 	reader := bufio.NewReader(os.Stdin)
+// 	// ReadString will block until the delimiter is entered
+// 	input, err := reader.ReadString('\n')
+// 	if err != nil {
+// 		fmt.Println("An error occured while reading input. Please try again", err)
+// 		return
+// 	}
+
+// 	// remove the delimeter from the string
+// 	input = strings.TrimSuffix(input, "\n")
+// 	fmt.Println(input)
+// }
